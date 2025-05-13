a8cfb0e7da80dd74ff296871532f76e5cdf37b7b
EquipoMetodosNumericos <racielr55@gmail.com>
2025-05-13 13:48:25 -0600

    Final Commit

--- js.js
+++ js.js
@@ -1,10 +1,18 @@
-function iniciarPrograma(){
-    document.getElementById('qr-code').style.display='none'
-    document.getElementById('qr').addEventListener('click',function(){
-        document.getElementById('qr-code').style.display='block'
+function iniciarPrograma() {
+    const fecha = new Date();
+    const dia = fecha.getDate();        
+    const mes = fecha.getMonth() + 1;    
+    const año = fecha.getFullYear();     
+
+    document.getElementById('fecha').innerHTML = `${dia}/${mes}/${año}`
+
+
+    document.getElementById('qr-code').style.display = 'none'
+    document.getElementById('qr').addEventListener('click', function () {
+        document.getElementById('qr-code').style.display = 'block'
     })
-    document.getElementById('c-qr').addEventListener('click',function(){
-        document.getElementById('qr-code').style.display='none'
+    document.getElementById('c-qr').addEventListener('click', function () {
+        document.getElementById('qr-code').style.display = 'none'
     })
 }
-window.addEventListener('load',iniciarPrograma)
+window.addEventListener('load', iniciarPrograma)
